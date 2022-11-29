20941c5401e128ce5514c5c46d218e48f657b902
hasnat123 <hasnat_45@hotmail.co.uk>
2022-11-29 14:27:04 +0000

    .

--- src/Components/PageContents/PageContents.tsx
+++ src/Components/PageContents/PageContents.tsx
@@ -12,7 +12,48 @@ import SectionStoreInfo from '../PageContents/Components/SectionStoreInfo/Sectio
 import HeroTwo from '../PageContents/Components/HeroTwo/HeroTwo';
 import HomeSectionCategory from '../../Routes/Home/Components/HomeSectionCategory/HomeSectionCategory';
 
-const PageContents: React.FC<{gender: string | null, hero1: string, hero2: string, hero3: string, category1: string, category2: string, category3: string, new1Pic: string, new1PicHover: string, new2Pic: string, new2PicHover: string, new3Pic: string, new3PicHover: string, new4Pic: string, new4PicHover: string, new5Pic: string, new5PicHover: string, new6Pic: string, new6PicHover: string, new1Price: string, new2Price: string, new3Price: string, new4Price: string, new5Price: string, new6Price: string, new1Title: string, new2Title: string, new3Title: string, new4Title: string, new5Title: string, new6Title: string, heroTwo1: string, heroTwo2: string, heroTwo3: string, brands1:string | null, brands2: string | null, home: boolean}> = ({gender, hero1, hero2, hero3, category1, category2, category3, new1Pic, new1PicHover, new2Pic, new2PicHover, new3Pic, new3PicHover, new4Pic, new4PicHover, new5Pic, new5PicHover, new6Pic, new6PicHover, new1Price, new2Price, new3Price, new4Price, new5Price, new6Price, new1Title, new2Title, new3Title, new4Title, new5Title, new6Title, heroTwo1, heroTwo2, heroTwo3, brands1, brands2, home}) => {
+interface IProps
+{
+  gender: string | null,
+  hero1: string,
+  hero2: string,
+  hero3: string,
+  category1: string,
+  category2: string,
+  category3: string,
+  new1Pic: string,
+  new1PicHover: string,
+  new2Pic: string,
+  new2PicHover: string,
+  new3Pic: string,
+  new3PicHover: string,
+  new4Pic: string,
+  new4PicHover: string,
+  new5Pic: string,
+  new5PicHover: string,
+  new6Pic: string,
+  new6PicHover: string,
+  new1Price: string,
+  new2Price: string,
+  new3Price: string,
+  new4Price: string,
+  new5Price: string,
+  new6Price: string,
+  new1Title: string,
+  new2Title: string,
+  new3Title: string,
+  new4Title: string,
+  new5Title: string,
+  new6Title: string,
+  heroTwo1: string,
+  heroTwo2: string,
+  heroTwo3: string,
+  brands1:string | null,
+  brands2: string | null,
+  home: boolean
+}
+
+const PageContents: React.FC<IProps> = ({gender, hero1, hero2, hero3, category1, category2, category3, new1Pic, new1PicHover, new2Pic, new2PicHover, new3Pic, new3PicHover, new4Pic, new4PicHover, new5Pic, new5PicHover, new6Pic, new6PicHover, new1Price, new2Price, new3Price, new4Price, new5Price, new6Price, new1Title, new2Title, new3Title, new4Title, new5Title, new6Title, heroTwo1, heroTwo2, heroTwo3, brands1, brands2, home}) => {
 
   return (
     <div className='page-contents'>
